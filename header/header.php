1bf8f9425b8e85fdc7ec38dcb72f403c8e2eb761
Johan <njoubert06@gmail.com>
2020-11-05 09:16:47 +0100

    add files php

--- header/header.php
+++ header/header.php
@@ -1,3 +1,49 @@
-<header>
-    
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles-header.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="fixed-top">
+
+    <div class="promo">
+        <p>mega promo</p>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+  <a class="navbar-brand" href="#"><img src="../images/logo.png" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="#">Boutique</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Découvrir
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item text-white" href="#">Action</a>
+          <a class="dropdown-item text-white" href="#">Another action</a>
+          <div class="dropdown-divider text-white"></div>
+          <a class="dropdown-item text-white" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled text-white" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</header> 
+</body>
+</html>
